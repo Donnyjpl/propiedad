e6a0e524c93218cd347027d93a990692e10041a5
donnyjpl <donnyjpl@gmail.com>
2024-07-17 13:00:37 -0400

    modelo normalizado

--- web/models.py
+++ web/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class Propiedad(models.Model):
-    TIPO_INMUEBLE_CHOICES = [
-        ('casa', 'Casa'),
-        ('departamento', 'Departamento'),
-        ('parcela', 'Parcela'),
-    ]
-
-    nombre = models.CharField(max_length=255)
-    descripcion = models.TextField()
-    m2_construidos = models.DecimalField(max_digits=10, decimal_places=2)
-    m2_terreno = models.DecimalField(max_digits=10, decimal_places=2)
-    num_estacionamientos = models.IntegerField()
-    num_habitaciones = models.IntegerField()
-    num_banos = models.IntegerField()
-    direccion = models.ForeignKey('Direccion', on_delete=models.SET_NULL, null=True)
-    tipo_inmueble = models.CharField(max_length=20, choices=TIPO_INMUEBLE_CHOICES)
-    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.nombre
-
 class Usuario(AbstractUser):
     RUT = models.CharField(max_length=12, unique=True)
     telefono_personal = models.CharField(max_length=15)
@@ -31,7 +10,6 @@ class Usuario(AbstractUser):
     ]
     tipo_usuario = models.CharField(max_length=12, choices=tipo_usuario_choices)
     direccion = models.ForeignKey('Direccion', on_delete=models.SET_NULL, null=True)
-    propiedades = models.ManyToManyField(Propiedad, related_name='usuarios', blank=True)
     
      # Define related_name único para los campos ManyToManyField
     groups = models.ManyToManyField(
@@ -52,18 +30,75 @@ class Usuario(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
-    
+class Propiedad(models.Model):
+    TIPO_INMUEBLE_CHOICES = [
+        ('casa', 'Casa'),
+        ('departamento', 'Departamento'),
+        ('parcela', 'Parcela'),
+    ]
 
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    m2_construidos = models.DecimalField(max_digits=10, decimal_places=2)
+    m2_terreno = models.DecimalField(max_digits=10, decimal_places=2)
+    num_estacionamientos = models.IntegerField()
+    num_habitaciones = models.IntegerField()
+    num_banos = models.IntegerField()
+    direccion = models.ForeignKey('Direccion', on_delete=models.SET_NULL, null=True)
+    tipo_inmueble = models.CharField(max_length=20, choices=TIPO_INMUEBLE_CHOICES)
+    precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
+    arrendador = models.ForeignKey(Usuario, related_name='propiedades_arrendadas', on_delete=models.CASCADE)
+    arrendatario = models.ForeignKey(Usuario, related_name='propiedades_arrendadas_a_mi', on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
     
+    
+class ImagenPropiedad(models.Model):
+    propiedad = models.ForeignKey(Propiedad, related_name='imagenes', on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='propiedades/')
+    descripcion = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Imagen de '{self.propiedad.nombre}'"    
+
+
+
+class SolicitudArriendo(models.Model):
+    ESTADO_CHOICES = [
+        ('pendiente', 'Pendiente'),
+        ('aprobada', 'Aprobada'),
+        ('rechazada', 'Rechazada'),
+    ]
+
+    propiedad = models.ForeignKey(Propiedad, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_solicitud = models.DateField(auto_now_add=True)
+    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default='pendiente')
+
+    def __str__(self):
+        return f"Solicitud de arriendo de {self.usuario} para {self.propiedad}"
+class Region(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    # Otros campos relevantes para la región
+
+    def __str__(self):
+        return self.nombre
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre}, {self.region}"   
+     
 class Direccion(models.Model):
     calle = models.CharField(max_length=255)
     numero = models.CharField(max_length=10)
     punto_referencia = models.CharField(max_length=255, blank=True, null=True)
-    comuna = models.CharField(max_length=100)
+    comuna = models.ForeignKey('Comuna', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         if self.punto_referencia:
-            return f"{self.calle} {self.numero}, {self.punto_referencia}, {self.comuna}"
+            return f"{self.calle} {self.numero}, {self.punto_referencia}, {self.comuna}, {self.region}"
         else:
-            return f"{self.calle} {self.numero}, {self.comuna}"
+            return f"{self.calle} {self.numero}, {self.comuna}, {self.region}"
--- web/services.py
+++ web/services.py
@@ -1,4 +1,4 @@
-from models import Usuario, Propiedad, Direccion
+from .models import Usuario, Propiedad, ImagenPropiedad, SolicitudArriendo, Region, Comuna, Direccion
 
 # Funciones CRUD para Usuario
 def crear_usuario(username, first_name, last_name, RUT, telefono_personal, tipo_usuario, direccion_id=None):
@@ -15,6 +15,9 @@ def crear_usuario(username, first_name, last_name, RUT, telefono_personal, tipo_
 
 def listar_usuarios():
     return Usuario.objects.all()
+
+def obtener_usuario_por_id(id):
+    return Usuario.objects.get(id=id)
 
 def actualizar_usuario(id, username=None, first_name=None, last_name=None, RUT=None, telefono_personal=None, tipo_usuario=None, direccion_id=None):
     usuario = Usuario.objects.get(id=id)
@@ -41,7 +44,7 @@ def borrar_usuario(id):
 
 
 # Funciones CRUD para Propiedad
-def crear_propiedad(nombre, descripcion, m2_construidos, m2_terreno, num_estacionamientos, num_habitaciones, num_banos, direccion_id, tipo_inmueble, precio_mensual):
+def crear_propiedad(nombre, descripcion, m2_construidos, m2_terreno, num_estacionamientos, num_habitaciones, num_banos, direccion_id, tipo_inmueble, precio_mensual, arrendador_id):
     propiedad = Propiedad.objects.create(
         nombre=nombre,
         descripcion=descripcion,
@@ -52,14 +55,18 @@ def crear_propiedad(nombre, descripcion, m2_construidos, m2_terreno, num_estacio
         num_banos=num_banos,
         direccion_id=direccion_id,
         tipo_inmueble=tipo_inmueble,
-        precio_mensual=precio_mensual
+        precio_mensual=precio_mensual,
+        arrendador_id=arrendador_id
     )
     return propiedad
 
 def listar_propiedades():
     return Propiedad.objects.all()
 
-def actualizar_propiedad(id, nombre=None, descripcion=None, m2_construidos=None, m2_terreno=None, num_estacionamientos=None, num_habitaciones=None, num_banos=None, direccion_id=None, tipo_inmueble=None, precio_mensual=None):
+def obtener_propiedad_por_id(id):
+    return Propiedad.objects.get(id=id)
+
+def actualizar_propiedad(id, nombre=None, descripcion=None, m2_construidos=None, m2_terreno=None, num_estacionamientos=None, num_habitaciones=None, num_banos=None, direccion_id=None, tipo_inmueble=None, precio_mensual=None, arrendador_id=None, arrendatario_id=None):
     propiedad = Propiedad.objects.get(id=id)
     if nombre:
         propiedad.nombre = nombre
@@ -81,6 +88,10 @@ def actualizar_propiedad(id, nombre=None, descripcion=None, m2_construidos=None,
         propiedad.tipo_inmueble = tipo_inmueble
     if precio_mensual:
         propiedad.precio_mensual = precio_mensual
+    if arrendador_id:
+        propiedad.arrendador_id = arrendador_id
+    if arrendatario_id is not None:
+        propiedad.arrendatario_id = arrendatario_id
     propiedad.save()
     return propiedad
 
@@ -89,12 +100,111 @@ def borrar_propiedad(id):
     propiedad.delete()
 
 
+# Funciones CRUD para ImagenPropiedad
+def crear_imagen_propiedad(propiedad_id, imagen, descripcion=''):
+    imagen_propiedad = ImagenPropiedad.objects.create(
+        propiedad_id=propiedad_id,
+        imagen=imagen,
+        descripcion=descripcion
+    )
+    return imagen_propiedad
+
+def listar_imagenes_propiedad(propiedad_id):
+    return ImagenPropiedad.objects.filter(propiedad_id=propiedad_id)
+
+def obtener_imagen_propiedad_por_id(id):
+    return ImagenPropiedad.objects.get(id=id)
+
+def actualizar_imagen_propiedad(id, descripcion=None):
+    imagen_propiedad = ImagenPropiedad.objects.get(id=id)
+    if descripcion:
+        imagen_propiedad.descripcion = descripcion
+    imagen_propiedad.save()
+    return imagen_propiedad
+
+def borrar_imagen_propiedad(id):
+    imagen_propiedad = ImagenPropiedad.objects.get(id=id)
+    imagen_propiedad.delete()
+
+
+# Funciones CRUD para SolicitudArriendo
+def crear_solicitud_arriendo(propiedad_id, usuario_id, estado='pendiente'):
+    solicitud = SolicitudArriendo.objects.create(
+        propiedad_id=propiedad_id,
+        usuario_id=usuario_id,
+        estado=estado
+    )
+    return solicitud
+
+def listar_solicitudes_arriendo():
+    return SolicitudArriendo.objects.all()
+
+def obtener_solicitud_arriendo_por_id(id):
+    return SolicitudArriendo.objects.get(id=id)
+
+def actualizar_solicitud_arriendo(id, estado=None):
+    solicitud = SolicitudArriendo.objects.get(id=id)
+    if estado:
+        solicitud.estado = estado
+    solicitud.save()
+    return solicitud
+
+def borrar_solicitud_arriendo(id):
+    solicitud = SolicitudArriendo.objects.get(id=id)
+    solicitud.delete()
+
+
+# Funciones CRUD para Region
+def crear_region(nombre):
+    region = Region.objects.create(nombre=nombre)
+    return region
+
+def listar_regiones():
+    return Region.objects.all()
+
+def obtener_region_por_id(id):
+    return Region.objects.get(id=id)
+
+def actualizar_region(id, nombre):
+    region = Region.objects.get(id=id)
+    region.nombre = nombre
+    region.save()
+    return region
+
+def borrar_region(id):
+    region = Region.objects.get(id=id)
+    region.delete()
+
+
+# Funciones CRUD para Comuna
+def crear_comuna(nombre, region_id):
+    comuna = Comuna.objects.create(nombre=nombre, region_id=region_id)
+    return comuna
+
+def listar_comunas():
+    return Comuna.objects.all()
+
+def obtener_comuna_por_id(id):
+    return Comuna.objects.get(id=id)
+
+def actualizar_comuna(id, nombre, region_id):
+    comuna = Comuna.objects.get(id=id)
+    comuna.nombre = nombre
+    comuna.region_id = region_id
+    comuna.save()
+    return comuna
+
+def borrar_comuna(id):
+    comuna = Comuna.objects.get(id=id)
+    comuna.delete()
+
+
 # Funciones CRUD para Direccion
-def crear_direccion(calle, numero, comuna, punto_referencia=None):
+def crear_direccion(calle, numero, comuna_id, punto_referencia=None):
     direccion = Direccion.objects.create(
         calle=calle,
         numero=numero,
-        comuna=comuna,
+        comuna_id=comuna_id,
         punto_referencia=punto_referencia
     )
     return direccion
@@ -102,14 +212,17 @@ def crear_direccion(calle, numero, comuna, punto_referencia=None):
 def listar_direcciones():
     return Direccion.objects.all()
 
-def actualizar_direccion(id, calle=None, numero=None, comuna=None, punto_referencia=None):
+def obtener_direccion_por_id(id):
+    return Direccion.objects.get(id=id)
+
+def actualizar_direccion(id, calle=None, numero=None, comuna_id=None, punto_referencia=None):
     direccion = Direccion.objects.get(id=id)
     if calle:
         direccion.calle = calle
     if numero:
         direccion.numero = numero
-    if comuna:
-        direccion.comuna = comuna
+    if comuna_id:
+        direccion.comuna_id = comuna_id
     if punto_referencia:
         direccion.punto_referencia = punto_referencia
     direccion.save()
@@ -118,7 +231,3 @@ def actualizar_direccion(id, calle=None, numero=None, comuna=None, punto_referen
 def borrar_direccion(id):
     direccion = Direccion.objects.get(id=id)
     direccion.delete()
-
-
-
-
